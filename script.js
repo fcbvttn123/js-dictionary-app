91deb228bde2f5b629c83127908767e1e3c65a51
David Vu <vutrant@Davids-MacBook-Pro.local>
2023-10-31 19:03:36 -0400

    Basic Function finished

--- script.js
+++ script.js
@@ -1,22 +1,68 @@
-getWordDetails("serene")
+const word_DOM = document.querySelector(".word");
+const phonetic_DOM = document.querySelector(".phonetic");
+const wordDetailBox_DOM = document.querySelector(".word-details");
+const mainForm = document.querySelector(".box form");
+
+mainForm.querySelector("button").addEventListener("click", e => {
+    e.preventDefault()
+    const searchedWord = mainForm.querySelector("input").value
+    getWordDetails(searchedWord);
+})
+
+
+
 
 function getWordDetails(word) {
   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`Word not found (Status Code: ${res.status})`);
+      }
+      return res.json();
+    })
     .then((data) => {
-        // word
-        console.log(`Word: ${data[0].word}`)
-
-        // audio 
-
-        // phonetic
-        console.log(`Phonetic: ${data[0].phonetic}`)
-
-        // word type (array)
-            // meaning (array)
-                // examples (array)
-        data[0].meanings.forEach(e => console.log(`Word Type: ${e.partOfSpeech}`))
+      word_DOM.textContent = data[0].word;
+      phonetic_DOM.textContent = data[0].phonetic;
+      data[0].meanings.forEach((e) => {
+        showContent(e)
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
 }
 
 
+
+
+// Create "word detail box" template
+function showContent(wordDetails) {
+    // Get Template
+    let temp = document.querySelector(".word-details-template");
+    const clone = document.importNode(temp.content, true);
+    // Place new text into template 
+    const wordType_DOM = clone.querySelector(".word-type");
+    wordType_DOM.textContent = wordDetails.partOfSpeech
+    // Create multiple definition in HTML p tag and put them into "HTML definition box"
+    wordDetails.definitions.forEach((e) =>{
+        const newP = document.createElement("p")
+        newP.classList.add("definition")
+        newP.textContent = `+) ${e.definition}`
+        const definitionBox_DOM = clone.querySelector(".definition-box");
+        definitionBox_DOM.appendChild(newP)
+    });
+    // Append new template
+    document.querySelector(".box__container2").appendChild(clone)
+}
+
+
+
+
+/*
+    Notes:
+    
+    + Bug: it does not remove the old "word detail" box when search for a new word
+
+    + Bug: Notification when not finding the right word 
+
+*/
